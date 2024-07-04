4a73a011e0877db2806ec43b23e31a02416fdbe7
HareemRehan13 <122636626+HareemRehan13@users.noreply.github.com>
2024-07-04 10:57:46 +0500

    afdrs

---
+++ WebApp_hareem/WebApp_hareem/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using WebApp_hareem.Models;
+
+namespace WebApp_hareem.Controllers
+{
+    public class HomeController : Controller
+    {
+       
+
+        public IActionResult Index()
+        {
+            if(HttpContext.Session.GetString("role") == "user")
+            {
+return View();
+            }
+            else
+            {
+                return RedirectToAction("Login", "Admin");
+            }
+        }
+
+      
+        public IActionResult AboutUs()
+        {
+            return View();
+        }
+
+		public IActionResult Products()
+		{
+			return View();
+		}
+		public IActionResult ContactUs()
+		{
+			return View();
+		}
+
+	}
+}
